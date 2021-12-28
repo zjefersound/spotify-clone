b60a121d7b12fbbc62943df6dc2115ec6f6b519f
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2021-12-28 13:39:53 -0300

    login page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import Sidebar from '../components/Sidebar'
 
 export default function Home() {
--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,30 @@
-function Login() {
+import { getProviders, signIn } from 'next-auth/react'
+
+function Login({ providers }) {
   return (
-    <div>
-      <h1>This is the Login page</h1>
+    <div className='bg-black h-screen overflow-hidden flex flex-col items-center justify-center'>
+      <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="Spotify" />
+      {Object.values(providers).map(provider => (
+        <div key={provider.name}>
+          <button
+            className='bg-[#18D860] text-white py-2 px-5 rounded-full'
+            onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+          >
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
     </div>
   )
 }
 
 export default Login
+
+export async function getServerSideProps({ }) {
+  const providers = await getProviders();
+  return {
+    props: {
+      providers
+    }
+  }
+}
