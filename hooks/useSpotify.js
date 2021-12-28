9936266daecf6f19853e33e556bb3bd682dd2582
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2021-12-28 14:59:32 -0300

    useSpotify hook

--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -1,5 +1,22 @@
-const useSpotify = () => {
+import { signIn, useSession } from "next-auth/react";
+import { useEffect } from "react";
+import spotifyApi from "../lib/spotify";
 
+const useSpotify = () => {
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    if (session) {
+      // If refresh token attempt fails, direct user to login...
+      if (session.error === "RefreshAccessTokenError") {
+        signIn();
+      }
+
+      spotifyApi.setAccessToken(session.user.accessToken);
+    }
+  }, [session]);
+
+  return spotifyApi;
 }
 
 export default useSpotify;
