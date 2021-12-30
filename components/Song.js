a32d2527db2522449f273910196aaa4103f34296
zjefersound <jeferson.2017317264@gmail.com>
2021-12-30 11:06:19 -0300

    playtrack feature

--- components/Song.js
+++ components/Song.js
@@ -1,12 +1,27 @@
+import { useRecoilState } from "recoil";
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import useSpotify from "../hooks/useSpotify"
 import { millisToMinutesAndSeconds } from "../lib/time";
 
 function Song({ order, track: data }) {
   const { track = {} } = data;
   const spotifyApi = useSpotify();
+  const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+
+  const playSong = () => {
+    setCurrentTrackId(track.id);
+    setIsPlaying(true);
+    spotifyApi.play({
+      uris: [track.uri]
+    })
+  }
 
   return (
-    <div className="grid grid-cols-2 text-gray-500 py-2 px-4 cursor-pointer hover:bg-gray-900 rounded-lg">
+    <div
+      onClick={playSong}
+      className="grid grid-cols-2 text-gray-500 py-2 px-4 cursor-pointer hover:bg-gray-900 rounded-lg"
+    >
       <div className="flex items-center space-x-4">
         <p>{order + 1}</p>
         <img className="h-10 w-10" src={track.album?.images[0].url} alt="" />
@@ -17,7 +32,7 @@ function Song({ order, track: data }) {
       </div>
       <div className="flex items-center justify-between ml-auto md:ml-8">
         <p className="w-40 hidden md:inline">{track.album?.name}</p>
-        <p>{millisToMinutesAndSeconds(track.duration_ms )}</p>
+        <p>{millisToMinutesAndSeconds(track.duration_ms)}</p>
       </div>
     </div>
   )
